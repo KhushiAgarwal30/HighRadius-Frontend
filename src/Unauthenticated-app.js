5f1947983585773c22e2cf34c8debeffd1aaaa44
sleepyArpan <arpanchattopadhyay23@gmail.com>
2020-11-26 10:59:03 +0530

    fixed code indentation

--- src/Unauthenticated-app.js
+++ src/Unauthenticated-app.js
@@ -1,21 +1,13 @@
-import {
-  InputLabel,
-  TextField,
-  Typography
-} from '@material-ui/core';
-import {
-  makeStyles
-} from '@material-ui/core/styles';
+import { InputLabel, TextField, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 import humanHand from './assets/human-machine-hand-homepage.svg';
-import {
-  UnaunthenticatedNavBar
-} from './components';
+import { UnaunthenticatedNavBar } from './components';
 
 const useStyles = makeStyles({
   flip: {
     transform: 'scale(-1,1)',
-    width: '100%'
+    width: '100%',
   },
   wrapper: {
     transform: 'translateY(-10%)',
@@ -45,102 +37,72 @@ const useStyles = makeStyles({
 
 export default function UnauthenticatedApp() {
   const styles = useStyles();
-  return ( <
-    >
-    <
-    UnaunthenticatedNavBar / >
-    <
-    div className = {
-      styles.wrapper
-    } >
-    <
-    img className = {
-      styles.flip
-    }
-    src = {
-      humanHand
-    }
-    alt = 'Human Hand' / >
-    <
-    /div> <
-    div className = {
-      styles.formWrapper
-    } >
-    <
-    Typography className = {
-      styles.targetText
-    }
-    variant = 'h4' >
-    ORDER MANAGEMENT APPLICATION <
-    /Typography> <
-    div className = {
-      styles.formStyles
-    } >
-    <
-    form >
-    <
-    Typography display = 'block'
-    style = {
-      {
-        marginBottom: '2rem',
-        color: '#666666'
-      }
-    }
-    variant = 'h5' >
-    Sign In <
-    /Typography> <
-    div style = {
-      {
-        marginBottom: '1rem'
-      }
-    } >
-    <
-    InputLabel style = {
-      {
-        marginBottom: '0.5rem'
-      }
-    }
-    htmlFor = 'username' >
-    Username <
-    /InputLabel> <
-    TextField fullWidth id = 'username'
-    style = {
-      {
-        background: '#DBF3FA',
-        borderRadius: '5px',
-        paddingTop: '0.75rem',
-      }
-    }
-    required /
-    >
-    <
-    /div> <
-    div >
-    <
-    InputLabel style = {
-      {
-        marginBottom: '0.5rem'
-      }
-    }
-    htmlFor = 'password' >
-    Password <
-    /InputLabel> <
-    TextField style = {
-      {
-        background: '#DBF3FA',
-        borderRadius: '5px',
-        paddingTop: '0.75rem',
-      }
-    }
-    fullWidth id = 'password'
-    type = 'password'
-    required /
-    >
-    <
-    /div> < /
-    form > <
-    /div> < /
-    div > <
-    />
+  return (
+    <>
+      <UnaunthenticatedNavBar />
+      <div className={styles.wrapper}>
+        <img className={styles.flip} src={humanHand} alt='Human Hand' />
+      </div>{' '}
+      <div className={styles.formWrapper}>
+        <Typography className={styles.targetText} variant='h4'>
+          ORDER MANAGEMENT APPLICATION{' '}
+        </Typography>{' '}
+        <div className={styles.formStyles}>
+          <form>
+            <Typography
+              display='block'
+              style={{
+                marginBottom: '2rem',
+                color: '#666666',
+              }}
+              variant='h5'>
+              Sign In{' '}
+            </Typography>{' '}
+            <div
+              style={{
+                marginBottom: '1rem',
+              }}>
+              <InputLabel
+                style={{
+                  marginBottom: '0.5rem',
+                }}
+                htmlFor='username'>
+                Username{' '}
+              </InputLabel>{' '}
+              <TextField
+                fullWidth
+                id='username'
+                style={{
+                  background: '#DBF3FA',
+                  borderRadius: '5px',
+                  paddingTop: '0.75rem',
+                }}
+                required
+              />
+            </div>{' '}
+            <div>
+              <InputLabel
+                style={{
+                  marginBottom: '0.5rem',
+                }}
+                htmlFor='password'>
+                Password{' '}
+              </InputLabel>{' '}
+              <TextField
+                style={{
+                  background: '#DBF3FA',
+                  borderRadius: '5px',
+                  paddingTop: '0.75rem',
+                }}
+                fullWidth
+                id='password'
+                type='password'
+                required
+              />
+            </div>{' '}
+          </form>{' '}
+        </div>{' '}
+      </div>{' '}
+    </>
   );
 }
